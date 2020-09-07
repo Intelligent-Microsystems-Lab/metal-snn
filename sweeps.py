196f02e60e1eb01b8fd24f239a7cad9384ef1aac
Clemens JS Schafer <clemens@Clemenss-MacBook-Pro.local>
2020-09-07 02:16:11 +0200

    const

--- sweeps.py
+++ sweeps.py
@@ -16,7 +16,7 @@ part4 = ".txt\nmodule load python\nsetenv OMP_NUM_THREADS $NSLOTS\npython train.
 #sweep_parameters = {'n-train':[10,15,25], 'train-samples':[200]}
 #sweep_parameters = {'checkpoint':['039b8454-1d57-4534-801a-6ae170dc368c','206b224c-20c8-41a3-884b-400d8a6b6677','2705f28d-5cf7-4b4e-8c4a-d1eeeee7108f','4dc5d819-c5a8-445e-aac3-db6cf90e484c','9b3bc513-e21f-4399-bae9-8220b32e2884','bd1ab2b1-7d98-45a2-8cb7-70fcccbccb18','f55bbde6-98ff-4fa0-be2b-ddaf6de7ac1c','f80d7f4a-0a32-4b5a-a2fe-380d9e9c0dae']}
 
-sweep_parameters = {'batch-size':[32]}
+sweep_parameters = {'batch-size':[128]}
 trials = 4
 
 #avail_q = ['gpu@qa-rtx6k-040.crc.nd.edu', 'gpu@qa-rtx6k-041.crc.nd.edu']
