963b6c25807f68b8b23eedfa5afd5bdde8a01f46
Clemens JS Schafer <clemens@Clemenss-MacBook-Pro.local>
2020-09-13 21:20:48 -0400

    new sweep

--- sweeps.py
+++ sweeps.py
@@ -1,6 +1,6 @@
 import os
 
-ident_word = "init-gain"
+ident_word = "tau-ref"
 
 part1 = "#!/bin/csh \n#$ -M cschaef6@nd.edu \n#$ -m abe\n#$ -q " 
 part11 = "\n#$ -l gpu_card=1\n#$ -N "
@@ -17,7 +17,7 @@ part4 = ".txt\nmodule load python\nsetenv OMP_NUM_THREADS $NSLOTS\npython train.
 #sweep_parameters = {'checkpoint':['039b8454-1d57-4534-801a-6ae170dc368c','206b224c-20c8-41a3-884b-400d8a6b6677','2705f28d-5cf7-4b4e-8c4a-d1eeeee7108f','4dc5d819-c5a8-445e-aac3-db6cf90e484c','9b3bc513-e21f-4399-bae9-8220b32e2884','bd1ab2b1-7d98-45a2-8cb7-70fcccbccb18','f55bbde6-98ff-4fa0-be2b-ddaf6de7ac1c','f80d7f4a-0a32-4b5a-a2fe-380d9e9c0dae']}
 
 #sweep_parameters = {'tau-mem-high':[12.5,27.5], 'tau-syn-high':[5,10], 'tau-ref-high':[0.1,0.5,2]}
-sweep_parameters = {'init-gain-conv1':[1e-08,1e-10,1e-12,1e-14]}
+sweep_parameters = {'tau-ref-high':[1,1.5,2,2.5]}
 trials = 2
 
 avail_q = ['gpu@qa-rtx6k-041.crc.nd.edu', 'gpu@qa-rtx6k-040.crc.nd.edu']
